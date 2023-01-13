d9bc8be6753cc70fb9eba4cfadb66ebbb6babe4f
deadrime <deadrime@yandex.ru>
2023-01-13 17:08:44 +0100

    Fix import for TextAreaInputWrapper

--- src/components/input/index.ts
+++ src/components/input/index.ts
@@ -1,10 +1,10 @@
 export { InputWrapper } from './InputWrapper'
 export { TextAreaInput } from './TextAreaInput'
 export type { TextAreaInputProps } from './TextAreaInput';
-export { TextAreaInputWrapper } from './TextAreaInputWrapper'
 export { TextInput } from './TextInput'
 export type { TextInputProps } from './TextInput'
 export { TextInputWrapper, PasswordInputWrapper } from './TextInputWrapper';
+export * from './TextAreaInputWrapper'
 export * from './types';
 export * from './CurrencyInput';
 export * from './CurrencyInputWrapper';
